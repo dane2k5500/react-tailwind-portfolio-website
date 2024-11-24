8d30c92c6f3efa121358a8be6bae5c9820756de4
Dani E. <60282095+dane2k5500@users.noreply.github.com>
2024-11-23 22:45:14 -0600

    Update Modal.jsx
    Made some slight edits

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,13 +12,13 @@ export default function Modal({open, onClose}) {
     {
       name: 'React Image Uploader with Express and Node.js', 
       link: 'https://dane2k5500.github.io/react-image-upload-app/', 
-      description: 'This project is an <strong>image uploading system</strong> built using React on the frontend and Express with Node.js on the backend. It enables users to upload images from their local device and securely store them on the server. Note: Since GitHub does not support server side code (e.g., the Node.js portion of this app), the front end will not work.  However, the frontend and backend code is provided in the repository!', 
+      description: 'This project is an image uploading system built using React on the frontend and Express with Node.js on the backend. It enables users to upload images from their local device and securely store them on the server. Note: Since GitHub does not support server side code (e.g., the Node.js portion of this app), the front end will not work.  However, the frontend and backend code is provided in the repository!', 
       github: 'https://github.com/dane2k5500/react-image-upload-app'
     },
     {
       name: 'Sign Up and Login Form with PHP, MySQL, HTML, CSS, and JavaScript', 
       link: 'https://dane2k5500.github.io/react-image-upload-app/', 
-      description: 'This project involves creating a **Sign-Up and Login Form** with robust client-side validation, focusing on security, usability, and functionality.  The form handles user registration and authentication while ensuring data integrity and a seamless user experience.', 
+      description: 'This project involves creating a sign-up and login form with robust client-side validation, focusing on security, usability, and functionality.  The form handles user registration and authentication while ensuring data integrity and a seamless user experience.', 
       github: 'https://github.com/dane2k5500/new_signup_form'
     },
   ]
